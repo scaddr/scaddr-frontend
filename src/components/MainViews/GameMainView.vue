319523eba36ee5d970466e54e04965614c2cd0c3
0xdeadbeer <64986162+0xdeadbeer@users.noreply.github.com>
2023-11-05 15:38:56 +0100

    GameMainView: remodel UI for the buttons

--- src/components/MainViews/GameMainView.vue
+++ src/components/MainViews/GameMainView.vue
@@ -1,29 +1,27 @@
 <template>
     <div class="grid grid-cols-4 gap-10 text-white text-center bg-opacity-95 rounded-lg w-fit hover:bg-opacity-100 transition shadow-black">
         <GameMenu class="col-span-1 px-16 py-32 bg-black rounded-lg shadow-xl"/>
-        <div :class="['relative h-full col-span-3 rounded-lg shadow-xl', isInterrogated ? 'bg-blue-700 text-light' : 'bg-black text-white']">
+        <div :class="['relative flex flex-col gap-0 grid-rows-2 h-full col-span-3 rounded-lg shadow-xl', isInterrogated ? 'bg-blue-700 text-light' : 'bg-black text-white']">
             <!-- top bar -->
-            <div class="z-50 absolute w-full border-b-white border border-t-0 border-l-0 border-r-0 rounded-t-lg text-left p-2">
+            <div class="border-b-white border border-t-0 border-l-0 border-r-0 rounded-t-lg text-left p-2">
                 <h1>Interrogating: {{events.question?.user ?? "Nobody"}}</h1>
             </div>
-            <div class="h-full flex items-center justify-center">
-                <div class="">
-                    <p :class="['select-none text-5xl m-auto w-fit p-5 rounded-lg', isInterrogated ? 'bg-white text-black' : 'bg-blue-600 text-white']">{{events.question?.question}}</p>
+            <div class="mt-10 pb-10">
+                <p :class="['select-none text-5xl m-auto mt-10 w-fit p-5 rounded-lg', isInterrogated ? 'bg-white text-black' : 'bg-blue-600 text-white']">{{events.question?.question}}</p>
 
-                    <div class="grid grid-cols-3 gap-3 mt-10" v-if="events.validation?.result == 'correct'">
-                        <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
-                            <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated, correctChoice: choice == events.validation?.userChoice}">{{choice}}</p>
-                        </div>
+                <div class="grid grid-rows-3 gap-3 mt-10" v-if="events.validation?.result == 'correct'">
+                    <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
+                        <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto w-fit min-w-[200px] select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated, correctChoice: choice == events.validation?.userChoice}">{{choice}}</p>
                     </div>
-                    <div class="grid grid-cols-3 gap-3 mt-10" v-else-if="events.validation?.result == 'wrong'"> 
-                        <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
-                            <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated, correctChoice: choice == events.validation?.correctAnswer, wrongChoice: choice == events.validation?.userChoice}">{{choice}}</p>
-                        </div>
+                </div>
+                <div class="grid grid-rows-3 gap-3 mt-10" v-else-if="events.validation?.result == 'wrong'"> 
+                    <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
+                        <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto w-fit min-w-[200px] select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated, correctChoice: choice == events.validation?.correctAnswer, wrongChoice: choice == events.validation?.userChoice}">{{choice}}</p>
                     </div>
-                    <div class="grid grid-cols-3 gap-3 mt-10" v-else>
-                        <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
-                            <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated}">{{choice}}</p>
-                        </div>
+                </div>
+                <div class="grid grid-rows-3 gap-3 mt-10" v-else>
+                    <div v-for="choice in events.question?.possibleAnswers" :key="choice" class="col-span-1">
+                        <p @click="isInterrogated ? sendAnswer(choice) : undefined" class="m-auto w-fit min-w-[200px] select-none bg-none border border-white p-2 rounded-lg text-white" :class="{'cursor-pointer hover:bg-white hover:text-black transition': isInterrogated}">{{choice}}</p>
                     </div>
                 </div>
             </div>
